e5dac61428f09bfa1c1a5488193db19687d2b771
Sackey <sackey.contact@gmail.com>
2018-02-05 17:54:04 +0100

    Cleaning files

--- Server/src/main/java/fr/univ_smb/isc/m2/config/SecurityConfig.java
+++ Server/src/main/java/fr/univ_smb/isc/m2/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package fr.univ_smb.isc.m2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,16 +21,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http.authorizeRequests()
-                .antMatchers(GET, "/api/**").anonymous()
-                .antMatchers(POST, "/api/**").authenticated()
-                .antMatchers(PUT, "/api/**").authenticated()
-                .antMatchers(DELETE, "/api/**").authenticated()
-                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-                .and()
-                .formLogin().usernameParameter("username").passwordParameter("password");
-
+            .antMatchers(GET, "/api/**").anonymous()
+            .antMatchers(POST, "/api/**").authenticated()
+            .antMatchers(PUT, "/api/**").authenticated()
+            .antMatchers(DELETE, "/api/**").authenticated()
+            .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+            .and()
+            .formLogin().usernameParameter("username").passwordParameter("password");
     }
 
 }
---
+++ Server/src/main/java/fr/univ_smb/isc/m2/domain/customer/Bottle.java
@@ -0,0 +1,4 @@
+package fr.univ_smb.isc.m2.domain.customer;
+
+public class Bottle {
+}
